aea973c8d79ed70dad7ee1b3c8f5b89c20d99876
Andrus Adamchik <andrus@objectstyle.com>
2015-12-11 17:15:00 +0300

    Guice Inject flavor

--- src/main/java/com/nhl/bootique/jersey/GuiceBridgeFeature.java
+++ src/main/java/com/nhl/bootique/jersey/GuiceBridgeFeature.java
@@ -1,10 +1,15 @@
 package com.nhl.bootique.jersey;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.ws.rs.core.Configuration;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 
+import org.glassfish.hk2.api.InjectionResolver;
 import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.api.TypeLiteral;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
 
@@ -13,6 +18,16 @@ import com.google.inject.Injector;
 public class GuiceBridgeFeature implements Feature {
 
 	static final String INJECTOR_PROPERTY = "com.nhl.bootique.jersey.injector";
+
+	static Injector getInjector(Configuration configuration) {
+		Injector injector = (Injector) configuration.getProperty(GuiceBridgeFeature.INJECTOR_PROPERTY);
+		if (injector == null) {
+			throw new IllegalStateException("Injector is not available in JAX RS runtime. Use property '"
+					+ GuiceBridgeFeature.INJECTOR_PROPERTY + "' to set it");
+		}
+
+		return injector;
+	}
 
 	private ServiceLocator hk2Container;
 
@@ -27,13 +42,18 @@ public class GuiceBridgeFeature implements Feature {
 
 		GuiceIntoHK2Bridge guiceBridge = hk2Container.getService(GuiceIntoHK2Bridge.class);
 
-		Injector injector = (Injector) context.getConfiguration().getProperty(INJECTOR_PROPERTY);
-		if (injector == null) {
-			throw new IllegalStateException(
-					"Injector is not available in JAX RS runtime. Use property '" + INJECTOR_PROPERTY + "' to set it");
-		}
+		Injector injector = getInjector(context.getConfiguration());
 
 		guiceBridge.bridgeGuiceInjector(injector);
+
+		// also allow Guice Inject flavor
+		context.register(new AbstractBinder() {
+			@Override
+			protected void configure() {
+				bind(GuiceInjectInjector.class).to(new TypeLiteral<InjectionResolver<com.google.inject.Inject>>() {
+				}).in(Singleton.class);
+			}
+		});
 
 		return true;
 	}
---
+++ src/main/java/com/nhl/bootique/jersey/GuiceInjectInjector.java
@@ -0,0 +1,63 @@
+package com.nhl.bootique.jersey;
+
+import java.lang.reflect.Member;
+
+import javax.ws.rs.core.Configuration;
+import javax.ws.rs.core.Context;
+
+import org.glassfish.hk2.api.Injectee;
+import org.glassfish.hk2.api.InjectionResolver;
+import org.glassfish.hk2.api.ServiceHandle;
+
+import com.google.inject.ConfigurationException;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+import com.google.inject.internal.Annotations;
+import com.google.inject.internal.Errors;
+import com.google.inject.internal.ErrorsException;
+
+@Singleton
+public class GuiceInjectInjector implements InjectionResolver<Inject> {
+
+	private Injector injector;
+
+	public GuiceInjectInjector(@Context Configuration configuration) {
+		this.injector = GuiceBridgeFeature.getInjector(configuration);
+	}
+
+	@Override
+	public boolean isConstructorParameterIndicator() {
+		return true;
+	}
+
+	@Override
+	public boolean isMethodParameterIndicator() {
+		return false;
+	}
+
+	@Override
+	public Object resolve(Injectee injectee, ServiceHandle<?> serviceHandle) {
+
+		if (injectee.getRequiredType() instanceof Class) {
+
+			TypeLiteral<?> typeLiteral = TypeLiteral.get(injectee.getRequiredType());
+			Errors errors = new Errors(injectee.getParent());
+			Key<?> key;
+			try {
+				key = Annotations.getKey(typeLiteral, (Member) injectee.getParent(),
+						injectee.getParent().getDeclaredAnnotations(), errors);
+			} catch (ErrorsException e) {
+				errors.merge(e.getErrors());
+				throw new ConfigurationException(errors.getMessages());
+			}
+
+			return injector.getInstance(key);
+		}
+
+		throw new IllegalStateException("Can't process injection point: " + injectee.getRequiredType());
+	}
+
+}
