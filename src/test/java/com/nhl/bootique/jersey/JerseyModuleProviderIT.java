2972716ffb053613aa30cd947cf7cd0849e77dde
Andrus Adamchik <andrus@objectstyle.com>
2016-03-17 14:47:44 -0400

    upgrading test api

--- src/test/java/com/nhl/bootique/jersey/JerseyModuleProviderIT.java
+++ src/test/java/com/nhl/bootique/jersey/JerseyModuleProviderIT.java
@@ -1,21 +1,13 @@
 package com.nhl.bootique.jersey;
 
-import static java.util.stream.Collectors.counting;
-import static org.junit.Assert.assertEquals;
-
-import java.util.ServiceLoader;
-import java.util.stream.StreamSupport;
-
 import org.junit.Test;
 
-import com.nhl.bootique.BQModuleProvider;
+import com.nhl.bootique.test.junit.BQModuleProviderChecker;
 
 public class JerseyModuleProviderIT {
 
 	@Test
 	public void testPresentInJar() {
-		long c = StreamSupport.stream(ServiceLoader.load(BQModuleProvider.class).spliterator(), false)
-				.filter(p -> p instanceof JerseyModuleProvider).collect(counting());
-		assertEquals("No provider found", 1, c);
+		BQModuleProviderChecker.testPresentInJar(JerseyModuleProvider.class);
 	}
 }
