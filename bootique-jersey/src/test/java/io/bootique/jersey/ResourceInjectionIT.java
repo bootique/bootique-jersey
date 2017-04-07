d7fec28d26187112e6eb10fb6e2845eebcc30f9b
Andrus Adamchik <andrus@objectstyle.com>
2017-04-07 14:22:16 +0300

    testing constructor injection into resources

--- bootique-jersey/src/test/java/io/bootique/jersey/ResourceInjectionIT.java
+++ bootique-jersey/src/test/java/io/bootique/jersey/ResourceInjectionIT.java
@@ -1,7 +1,6 @@
 package io.bootique.jersey;
 
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import io.bootique.jetty.JettyModule;
 import io.bootique.jetty.test.junit.JettyTestFactory;
 import org.glassfish.jersey.client.ClientConfig;
@@ -30,57 +29,139 @@ public class ResourceInjectionIT {
     @ClassRule
     public static JettyTestFactory JETTY_FACTORY = new JettyTestFactory();
 
+    private static InjectedService SERVICE;
+
     private Client client;
 
     @BeforeClass
     public static void startJetty() throws InterruptedException, ExecutionException, TimeoutException {
 
+        SERVICE = new InjectedService();
+
         JETTY_FACTORY.app()
                 .modules(JettyModule.class, JerseyModule.class)
                 .module(binder -> {
-                    binder.bind(InjectedService.class).in(Singleton.class);
-                    JerseyModule.extend(binder).addResource(InjectedResource.class);
+                    binder.bind(InjectedService.class).toInstance(SERVICE);
+                    JerseyModule.extend(binder)
+                            .addResource(FieldInjectedResource.class)
+                            .addResource(ConstructorInjectedResource.class)
+                            .addResource(UnInjectedResource.class);
+
+                    binder.bind(UnInjectedResource.class).toProvider(() -> new UnInjectedResource(SERVICE));
+
                 }).start();
     }
 
     @Before
     public void before() {
+
+        SERVICE.reset();
+
         ClientConfig config = new ClientConfig();
         this.client = ClientBuilder.newClient(config);
     }
 
     @Test
-    public void testResponse() {
+    public void testFieldInjected() {
 
-        WebTarget target = client.target("http://127.0.0.1:8080/");
+        WebTarget target = client.target("http://127.0.0.1:8080/f");
 
         Response r1 = target.request().get();
         assertEquals(Status.OK.getStatusCode(), r1.getStatus());
-        assertEquals("_1", r1.readEntity(String.class));
+        assertEquals("f_1", r1.readEntity(String.class));
         r1.close();
 
         Response r2 = target.request().get();
         assertEquals(Status.OK.getStatusCode(), r2.getStatus());
-        assertEquals("_2", r2.readEntity(String.class));
+        assertEquals("f_2", r2.readEntity(String.class));
         r2.close();
     }
 
-    @Path("/")
+    @Test
+    public void testConstructorInjected() {
+
+        WebTarget target = client.target("http://127.0.0.1:8080/c");
+
+        Response r1 = target.request().get();
+        assertEquals(Status.OK.getStatusCode(), r1.getStatus());
+        assertEquals("c_1", r1.readEntity(String.class));
+        r1.close();
+
+        Response r2 = target.request().get();
+        assertEquals(Status.OK.getStatusCode(), r2.getStatus());
+        assertEquals("c_2", r2.readEntity(String.class));
+        r2.close();
+    }
+
+    @Test
+    public void testProviderForResource() {
+
+        WebTarget target = client.target("http://127.0.0.1:8080/u");
+
+        Response r1 = target.request().get();
+        assertEquals(Status.OK.getStatusCode(), r1.getStatus());
+        assertEquals("u_1", r1.readEntity(String.class));
+        r1.close();
+
+        Response r2 = target.request().get();
+        assertEquals(Status.OK.getStatusCode(), r2.getStatus());
+        assertEquals("u_2", r2.readEntity(String.class));
+        r2.close();
+    }
+
+    @Path("/f")
     @Produces(MediaType.TEXT_PLAIN)
-    public static class InjectedResource {
+    public static class FieldInjectedResource {
 
         @Inject
         private InjectedService service;
 
         @GET
         public String get() {
-            return "_" + service.getNext();
+            return "f_" + service.getNext();
+        }
+    }
+
+    @Path("/c")
+    @Produces(MediaType.TEXT_PLAIN)
+    public static class ConstructorInjectedResource {
+
+        private InjectedService service;
+
+        @Inject
+        public ConstructorInjectedResource(InjectedService service) {
+            this.service = service;
+        }
+
+        @GET
+        public String get() {
+            return "c_" + service.getNext();
+        }
+    }
+
+    @Path("/u")
+    @Produces(MediaType.TEXT_PLAIN)
+    public static class UnInjectedResource {
+
+        private InjectedService service;
+
+        public UnInjectedResource(InjectedService service) {
+            this.service = service;
+        }
+
+        @GET
+        public String get() {
+            return "u_" + service.getNext();
         }
     }
 
     public static class InjectedService {
 
         private AtomicInteger atomicInt = new AtomicInteger();
+
+        public void reset() {
+            atomicInt.set(0);
+        }
 
         public int getNext() {
             return atomicInt.incrementAndGet();
