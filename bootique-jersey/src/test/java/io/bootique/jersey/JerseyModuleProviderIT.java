5237418bd1691c754a17d3b149377d61dfaa4a17
Andrus Adamchik <andrus@objectstyle.com>
2018-02-18 10:27:42 +0300

    Declare module dependencies bootique/bootique#203
    * new test API

--- bootique-jersey/src/test/java/io/bootique/jersey/JerseyModuleProviderIT.java
+++ bootique-jersey/src/test/java/io/bootique/jersey/JerseyModuleProviderIT.java
@@ -3,11 +3,10 @@ package io.bootique.jersey;
 import io.bootique.BQRuntime;
 import io.bootique.jetty.JettyModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQRuntimeChecker;
 import io.bootique.test.junit.BQTestFactory;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static com.google.common.collect.ImmutableList.of;
 
 public class JerseyModuleProviderIT {
 
@@ -22,9 +21,9 @@ public class JerseyModuleProviderIT {
     @Test
     public void testModuleDeclaresDependencies() {
         final BQRuntime bqRuntime = testFactory.app().module(new JerseyModuleProvider()).createRuntime();
-        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(
+        BQRuntimeChecker.testModulesLoaded(bqRuntime,
                 JettyModule.class,
                 JerseyModule.class
-        ));
+        );
     }
 }
