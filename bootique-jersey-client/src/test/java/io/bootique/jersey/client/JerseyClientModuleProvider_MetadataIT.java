9cc9066c6fff217058fbfa47fb98c6dc32b565d7
Andrus Adamchik <andrus@objectstyle.com>
2023-11-25 08:48:27 -0500

    (Jakarta migration) Deprecate JAX-RS 2 modules #84

--- bootique-jersey-client/src/test/java/io/bootique/jersey/client/JerseyClientModuleProvider_MetadataIT.java
+++ bootique-jersey-client/src/test/java/io/bootique/jersey/client/JerseyClientModuleProvider_MetadataIT.java
@@ -52,7 +52,7 @@ public class JerseyClientModuleProvider_MetadataIT {
         assertTrue(jerseyClientOpt.isPresent());
         ModuleMetadata jerseyClient = jerseyClientOpt.get();
 
-        assertEquals("Integrates Jersey JAX-RS HTTP client.", jerseyClient.getDescription());
+        assertEquals("Deprecated and should be replaced with 'bootique-jersey-jakarta-client'.", jerseyClient.getDescription());
 
         assertEquals(1, jerseyClient.getConfigs().size());
         ConfigMetadataNode rootConfig = jerseyClient.getConfigs().stream().findFirst().get();
